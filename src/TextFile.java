6f8c063443b7e6987ff0351ed5064ef9f423a853
risc604 <risc604@ms40.url.com.tw>
2016-05-28 22:43:18 +0800

    finish

--- src/TextFile.java
+++ src/TextFile.java
@@ -1,90 +1,28 @@
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import	java.lang.*;
-import	java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
-@SuppressWarnings("unused")
+
 public class TextFile
 {
-	final static String	fileName = "mlcDevices.txt";
-	static List<String>	deviceList = new ArrayList<>();	
-	
-	public TextFile()
-	{
-	}
-	
-	//public static void createDefaultFile(String fileName)
-	private void createDefaultFile(String fileName)
-	{
-		try
-		{
-			BufferedWriter outputText = new BufferedWriter(new FileWriter(fileName));
-			outputText.write("3MW1-4B,11 ");
-			outputText.newLine();
-			outputText.write("A6 BT, 12");
-			outputText.newLine();
-			outputText.write("BP3GT1-6B, 12 ");
-			outputText.newLine();
-					
-			outputText.close();
-		}
-		catch (IOException ioError)
-		{
-			ioError.printStackTrace();
-		}
-	}
-	
-	public boolean readTextFile(String fileName)
-	{
-		try
-		{
-			BufferedReader	inputText = new BufferedReader(new FileReader(fileName));
-			String	tmpInfo = null;
-			
-			while((tmpInfo = inputText.readLine()) != null )
-			{
-				deviceList.add(tmpInfo);
-			}
-			inputText.close();
-			return true;
-		}
-		catch(IOException ioError)
-		{
-			System.out.println("file not exist. to create default mlcDevice.txt by default devices !!");
-			//createDefaultFile(fileName);
-			return false;
-		}
-	}
-	
-	public void displayList(List<String> listData) 
-	{
-		if(listData.isEmpty())
-			return;
-		
-		for(int i=0; i<listData.size(); i++)
-		{
-			System.out.println("Data[" + i + "]= " + listData.get(i));
-		}
-	}
-	
-	
+	private final static String	fileName = "mlcDevices.txt";
+	private static List<String>	deviceList = new ArrayList<>();	
+	private static List<String>	nameList = new ArrayList<>();	
 	
 	public static void main(String[] args)
 	{
-		TextFile	textObj = new TextFile();
+		//TextFile	textObj = new TextFile();
+		TextFileEvent	textFileObj = new TextFileEvent();
 		
-		if(!textObj.readTextFile(fileName))
+		if(!textFileObj.readTextFile(fileName, deviceList))
 		{
-			TextFile.createDefaultFile(fileName);
-			TextFile.readTextFile(fileName);
+			textFileObj.createDefaultFile(fileName);
+			textFileObj.readTextFile(fileName, deviceList);
 		}
 		
-		displayList(deviceList);
+		textFileObj.displayList(deviceList);
+		nameList = textFileObj.getDeviceNameList(deviceList);
 		
 		System.out.println("Hello, Java !!");
 	}
